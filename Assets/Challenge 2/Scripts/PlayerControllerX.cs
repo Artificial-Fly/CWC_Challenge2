135badac3ad91ae829dbf693c7d3430ca740d092
Average942 <lazyfruit1@hotmail.com>
2023-07-31 19:37:17 +0900

    done
    Unit 2 practical is done

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public bool dogAvailable = true;
+    /// Start is called on the frame when a script is enabled just before
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&&dogAvailable)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            dogAvailable = false;
+            StartCoroutine(DogSpawn());
+        }
+        
+    }
+
+    IEnumerator DogSpawn(){
+        yield return new WaitForSeconds(2.5f);
+        dogAvailable = true;
+    }
+}
